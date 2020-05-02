826ee1c46f5a778c36b6ff20344f5157c7449724
Michael Ledesma <michael.ledesma808@gmail.com>
2020-05-02 13:39:12 -0700

    includes

---
+++ home/home.html.php
@@ -0,0 +1,20 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+  <title>Ace In The Hole</title>
+  <meta name="description" content="">
+  <meta charset="utf-8">
+  <meta name="viewport" content="width=device-width, initial-scale=1.0">
+  <link rel="stylesheet" href="../style/css/project.css">
+</head>
+<body>
+  <div id="wrapper">
+    <?php include 'includes/header.inc.html.php'?>
+    <?php include 'includes/nav.inc.html.php' ?>
+    <main>
+
+    </main>
+    <?php include 'includes/footer.inc.html.php' ?>
+  </div>
+</body>
+</html>
