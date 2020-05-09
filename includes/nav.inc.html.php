a0a6fc7ddb559b3a37f57ce5fc866f57a628caf4
Michael Ledesma <michael.ledesma808@gmail.com>
2020-05-09 12:28:34 -0700

    nav update

--- includes/nav.inc.html.php
+++ includes/nav.inc.html.php
@@ -1,11 +1,6 @@
 <?php define ("BASE_URL", "aith/") ?>
 <nav>
     <a href="<?php echo BASE_URL; ?>" class="active ">AITH</a>
-    <a href="<?php echo BASE_URL; ?>event" class="navLinks">Event</a>
-    <a href="<?php echo BASE_URL; ?>showcase" class="navLinks">Showcase</a>
-    <a href="<?php echo BASE_URL; ?>registration" class="navLinks">Registration</a>
-    <a href="<?php echo BASE_URL; ?>faq" class="navLinks">FAQ</a>
-    <a href="<?php echo BASE_URL; ?>contact" class="navLinks">Contact</a>
     <div id="burger">
         <a href="<?php echo BASE_URL; ?>event" class="inactive">Event</a>
         <a href="<?php echo BASE_URL; ?>showcase" class="inactive">Showcase</a>
@@ -14,19 +9,25 @@
         <a href="<?php echo BASE_URL; ?>contact" class="inactive">Contact</a>
     </div>
 <!-- "Hamburger menu" / "Bar icon" to toggle the navigation links -->
-    <a href="javascript:void(0);" class="icon" onclick="myFunction()">
+    <a href="javascript:void(0);" class="icon" onclick="myFunction(x)">
     <i class="fa fa-bars"></i>
     </a>
 </nav>
 <script>
 
-function myFunction(){
-var inactiveLinks = document.getElementById("burger");
-  if (inactiveLinks.style.display === "block") {
-    inactiveLinks.style.display = "none";
-  } else {
-    inactiveLinks.style.display = "block";
+function myFunction(x){
+
+  var inactiveLinks = document.getElementById("burger");
+
+  if (x.matches) {
+    if (inactiveLinks.style.display === "block") {
+      inactiveLinks.style.display = "none";
+    } else {
+      inactiveLinks.style.display = "block";
+    }
   }
 }
+
+var x = window.matchMedia("(max-width: 900px)")
 
 </script>
