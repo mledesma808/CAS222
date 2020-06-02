437fc5676815fb373e29d394438a0cdeb0d2c23d
Michael Ledesma <michael.ledesma808@gmail.com>
2020-06-02 15:27:38 -0700

    home completed

--- home/home.html.php
+++ home/home.html.php
@@ -2,21 +2,73 @@
 <html lang="en">
 <head>
   <title>Ace In The Hole</title>
-  <meta name="description" content="">
+  <meta name="description" content="Ace In The Hole Multisports Events offers events for Every Body.">
   <meta charset="utf-8">
   <meta name="viewport" content="width=device-width, initial-scale=1.0">
-  <link rel="stylesheet" href="/aith/style/css/reset.css">
-  <!-- Load an icon library to show a hamburger menu (bars) on small screens -->
-  <link rel="stylesheet" href="https://cdnjs.cloudflare.com/ajax/libs/font-awesome/4.7.0/css/font-awesome.min.css">
-  <link rel="stylesheet" href="/aith/style/css/style.css">
+  <?php include 'includes/link.inc.html.php' ?>
 </head>
 <body>
   <div id="wrapper">
-    <?php include 'includes/nav.inc.html.php' ?>
-    <main>
+    <?php include 'includes/nav.inc.html.php'?>
+    <main class="grid">
+      <article class="boxOne">
+        <h1>ACE IN THE HOLE MULTISPORT EVENT</h1>
+        <img src="images/original/ace6.jpg" alt="Three athelets standing arm in arm on a beach." class="banner">
+      </article>
+      <article class="boxTwo">
+        <h2>About AITH Events</h2>
+        <p>Ace in the Hole Multisport Events is proud to offer running and triathlon events to athletes of all shapes and sizes, national origins, gender identifications and cultural backgrounds. We offer Events for Every Body.</p>
+      </article>
+      <article class="boxThree">
+        <h2>About the Event</h2>
+        <p>The Annual Ace in the Hole Multisport Weekend is a legendary event in the Oregon triathlon and running community. It has become a traditional destination race for athletes from across the nation.</p>
+        <p>There is something for every level of athletic ability. The weekend includes a first timer triathlon, a sprint, Olympic, and Half-Iron triathlons and 10K and Half marathon runs. Come to experience your first race or come to compete to win, but make sure you come to have fun!</p>
+      </article>
+      <aside class="boxFour">
+        <img src="<?php echo BASE_URL; ?>images/original/ace11.jpg" class="imageBox">
+      </aside>
+      <article class="boxFive">
+        <h2>Starting Times</h2>
+        <section>
+          <h3>Saturday</h3>
+          <p><span class="bigBold">Long Course Triathlon:</span> 7:00 AM</p>
+          <p><span class="bigBold">Olympic Triathlon:</span> 7:30 AM</p>
+          <p><span class="bigBold">10K:</span> 7:15 AM</p>
+          <p><span class="bigBold">Half Marathon:</span> 7:15 AM</p>
+        </section>
+        <section>
+          <h3>Sunday</h3>
+          <p><span class="bigBold">Sprint Triathlon:</span> 8:00 AM</p>
+          <p><span class="bigBold">Try-a-Tri:</span> 8:20 AM</p>
+          <p><span class="bigBold">Splash n Dash:</span> 12:00 PM</p>
+        </section>
+      </article>
+      <article class="boxSix">
+        <h2>PACKET PICK UP</h2>
+        <p>All packet pick up will occur at Why Worry Racing, 123 NW Everett, Portland OR. Packet pick up hours: Thursday 8-5, Friday 9-noon. No day of event packet pick up.</p>
+        <section>
+          <h3>COURSE DETAILS</h3>
+          <p><span class="bigBold">WATER TEMPERATURE</span> is expected to be between 62 & 66 degrees. The temperature will be taken on Friday and the morning of the race. Wetsuits are recommended.</p>
+          <p><span class="bigBold">Long Course SWIM - 1.2mi -</span> Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+          <p><span class="bigBold">Long Course BIKE - 58 Miles:</span> A scenic point-to-point course that travels over gently rolling hills prior to three hard climbs. The bike course will be marked with large directional signage and there will be course marshals at key intersections to help direct you.</p>
+          <p><span class="bigBold">Long Course RUN - 13.1mi -</span> A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (two hills total with a minimal elevation gain).</p>
+          <p><span class="bigBold">OLYMPIC SWIM - 1,500 meters -</span> Participants will make two counter-clockwise loops. Large buoys will mark the turn points. Kayakers will be positioned on the water to support the swimmers. Medical support will be present on the beach.</p>
+          <p><span class="bigBold">OLYMPIC BIKE - 28mi -</span> A scenic point-to-point course that travels over gently rolling hills. The bike course will be marked with large directional signage and course marshals will be present at key intersections.</p>
+          <p><span class="bigBold">OLYMPIC RUN - 10K -</span> A mostly flat loop course on widely paved bike paths that traverse through and around this beautiful and scenic destination resort (one hill total with a minimal elevation gain).</p>
+          <p><span class="bigBold">Sprint:</span> Course will offer a 1-loop 1/2 mile swim, the exact same 28 mile Bike Course as the Olympic distance and a 5km run.</p>
+          <p><span class="bigBold">Try-A-Tri:</span> This novice race is designed for the first time triathlete, those new to the sport, our Junior Triathletes. The swim is a shorter, more manageable 1/4 mile distance, (instead of the standard 1/2-mile Sprint distance swim), 10 mile bike ride (vs 12 miles and it's a 2 loop course making it very spectator friendly!) and a flat 2 mile run (vs 3 mile sprint course).</p>
+          <p><span class="bigBold">Half Marathon COURSE:</span> Half-Marathon (13.1-miles): Half-Marathon event starts and finishes in the Athletes Village to the cheers of the enthusiastic crowd. Once finished, runners can enjoy the finish line festivities, including the Sports & Fitness Expo and live entertainment. Post-race refreshments will be provided and the Awards Ceremony for the Half-Marathon will begin once the results have been certified</p>
+          <p><span class="bigBold">10k COURSE:</span> The 10K event starts and finishes in the Athletes Village. The paths are approximately 6 ft wide, perfectly paved and wind around through the forest. Each course has only two small hills with a minimal elevation gain to navigate and a fast downhill to flat finish to the roaring cheers of the crowd.</p>
+          <p><span class="bigBold">Splash n' Dash:</span> Kids event for participants ages 4 to 15. This is a 100 yard swim and a 50 yard dash. It is free if an adult registers for an event otherwise it is $25.</p>
+        </section>
+      </article>
+
+      <?php include 'includes/facebook.inc.html.php' ?>
+      <?php include 'includes/weather.inc.html.php' ?>
+      <?php include 'includes/twitter.inc.html.php' ?>
 
     </main>
-    <?php include 'includes/footer.inc.html.php' ?>
+    <?php include 'includes/footer.inc.html.php'?>
   </div>
 </body>
 </html>
